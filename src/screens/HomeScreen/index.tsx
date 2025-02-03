8d6b31bc64e97c19b6a8030e96635aeaaadd7c0b
Cenk Aktas <cnktass@gmail.com>
2025-02-03 21:23:28 +0300

    MessageNotification

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -4,6 +4,7 @@ import productassets from '../../../assets/products';
 import FavoriteProducts from '../../components/FavoriteProducts';
 import MainProducts from "../../components/MainProducts"
 import CategoryFilter from "../../components/CategoryFilter"
+import MessageNotification from "../../components/MessageNotification"
 import { Product } from '../../models/index';
 import styles from './styles';
 
@@ -16,6 +17,7 @@ function Index() {
 
   return (
     <ScrollView style={{backgroundColor:'white',height:'100%'}}>
+      <MessageNotification/>
       <CategoryFilter/>
       <FavoriteProducts/>
       <MainProducts filtered={false} header={"Pendik"} mainProducts={products} />
